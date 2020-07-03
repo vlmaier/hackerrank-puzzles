2efca7cb5c41ad6b291b91615dbbe10bc270a8d5
Vladas Maier <v.maier@dracoon.com>
2020-07-03 13:56:59 +0200

    Add "Staircase" solution

---
+++ puzzles/problem_solving/Staircase.java
@@ -0,0 +1,29 @@
+package problem_solving;
+
+import java.util.*;
+
+
+public class Staircase {
+
+    static void staircase(int n) {
+        for (int i = 1; i <= n; i++) {
+            // Java 8 solution
+            String whitespace = String.join("", Collections.nCopies(n - i, " "));
+            String hash = String.join("", Collections.nCopies(i, "#"));
+            System.out.println(whitespace + hash);
+            // Java 11 solution
+            // System.out.println(" ".repeat(n - i) + "#".repeat(i));
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        staircase(n);
+
+        scanner.close();
+    }
+}
