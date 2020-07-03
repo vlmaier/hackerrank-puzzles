0a6ff123c546aa8d2b5e153a412214fbd5f28219
Vladas Maier <v.maier@dracoon.com>
2020-07-03 15:32:10 +0200

    Add "Birthday Cake Candles" solution

---
+++ puzzles/problem_solving/BirthdayCakeCandles.java
@@ -0,0 +1,41 @@
+package problem_solving;
+
+import java.io.*;
+import java.util.*;
+
+
+public class BirthdayCakeCandles {
+
+    static int birthdayCakeCandles(int[] array) {
+        int max = Arrays.stream(array).max().orElse(0);
+        return (int) Arrays.stream(array).filter(i -> i == max).count();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = birthdayCakeCandles(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
