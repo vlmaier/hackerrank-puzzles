faa5d1bfc4e8ab40a6e12413e0cc3539cf7025ed
Vladas Maier <v.maier@dracoon.com>
2020-07-03 17:43:01 +0200

    Add "Divisible Sum Pairs" solution

---
+++ puzzles/problem_solving/DivisibleSumPairs.java
@@ -0,0 +1,51 @@
+package problem_solving;
+
+import java.io.*;
+import java.util.*;
+
+
+public class DivisibleSumPairs {
+
+    static int divisibleSumPairs(int n, int k, int[] array) {
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (i != j && (array[i] + array[j]) % k == 0) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] array = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            array[i] = arItem;
+        }
+
+        int result = divisibleSumPairs(n, k, array);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
