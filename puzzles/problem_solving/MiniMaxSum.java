267bdba3e794eb126d8923aaf6aadb9ac2c0d652
Vladas Maier <v.maier@dracoon.com>
2020-07-03 15:18:44 +0200

    Add "Mini-Max Sum" solution

---
+++ puzzles/problem_solving/MiniMaxSum.java
@@ -0,0 +1,34 @@
+package problem_solving;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class MiniMaxSum {
+
+    static void miniMaxSum(int[] arr) {
+        long[] origin = Arrays.stream(arr).mapToLong(i -> i).toArray();
+        long minValue = Arrays.stream(origin).min().orElse(0);
+        long maxValue = Arrays.stream(origin).max().orElse(0);
+        long sum = Arrays.stream(origin).sum();
+        System.out.printf("%d %d%n", sum - maxValue, sum - minValue);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
