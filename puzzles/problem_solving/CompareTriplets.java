93527ba902b99b5234ebc247f63d891652566661
Vladas Maier <v.maier@dracoon.com>
2020-07-03 13:45:25 +0200

    Add "Compare Triplets" solution

---
+++ puzzles/problem_solving/CompareTriplets.java
@@ -0,0 +1,52 @@
+package problem_solving;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+public class CompareTriplets {
+
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        List<Integer> answer = new ArrayList<>();
+        Integer aPoints = 0;
+        Integer bPoints = 0;
+        for (int i = 0; i <= 2; i++) {
+            if (a.get(i) > b.get(i)) {
+                aPoints++;
+            } else if (a.get(i) < b.get(i)) {
+                bPoints++;
+            }
+        }
+        answer.add(aPoints);
+        answer.add(bPoints);
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter =  new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = compareTriplets(a, b);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
